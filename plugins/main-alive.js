447af5668d4ad70b272a62179e0eaaa6242544fa
MALIK <hasnainthedarklover@gmail.com>
2024-08-15 17:15:24 +0500

    Update main-alive.js

--- plugins/main-alive.js
+++ plugins/main-alive.js
@@ -4,12 +4,12 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
   var vn = 'https://cdn.jsdelivr.net/gh/Lazack28/Lazack-md@main/media/Alive.mp3'
   let url = 'https://github.com/Lazack28/Lazack-md'
   let murl = 'https://youtu.be/3j_EIP--2t8?si=4TFWV0On6Bl1wr-e'
-  let img = 'https://i.imgur.com/QMyKIPq.jpeg'
+  let img = 'https://i.imgur.com/drAZuH2.jpeg'
   let con = {
     key: {
       fromMe: false,
       participant: `${m.sender.split`@`[0]}@s.whatsapp.net`,
-      ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}),
+      ...(m.chat ? { remoteJid: '923263429027@s.whatsapp.net' } : {}),
     },
     message: {
       contactMessage: {
@@ -24,16 +24,16 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     },
     mimetype: 'audio/mpeg',
     ptt: true,
-    waveform: [100, 0, 100, 0, 100, 0, 100],
+    waveform: [0, 25, 50, 100, 50, 25, 0],
     fileName: 'shizo',
 
     contextInfo: {
       mentionedJid: [m.sender],
       externalAdReply: {
-        title: 'LAZACK MD ALIVE',
+        title: '𝗠𝗔𝗟𝗜𝗞 is ALIVE',
         body: 'Lazack md',
         thumbnailUrl: img,
-        sourceUrl: 'https://chat.whatsapp.com/IIpL6gf6dcq4ial8gaJLE9',
+        sourceUrl: 'https://chat.whatsapp.com/KrJhpZhgKxZLfHcyUZKOMw',
         mediaType: 1,
         renderLargerThumbnail: true,
       },
